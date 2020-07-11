c468645cadb8cb4edd5ce362a1443017578ee9f6
Mark Whang <hwhang0917@gmail.com>
2020-07-11 13:13:32 +0900

    [2.1] useTitle hook

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React, { useState, useEffect } from "react";
+import { useTitle } from "./hooks/useTitle";
 
 const App = () => {
-  const sayHello = () => console.log("Hello World!");
-  const [number, setNumber] = useState(0);
-  const [Anumber, setAnumber] = useState(0);
-  useEffect(sayHello, [number]);
+  const titleUpdater = useTitle("Loading...");
+  setTimeout(() => {
+    titleUpdater("Home");
+  }, 3000);
   return (
     <div className="App">
-      <h1>Hello Numbers</h1>
-      <button onClick={() => setNumber(number + 1)}>{number}</button>
-      <button onClick={() => setAnumber(Anumber + 1)}>{Anumber}</button>
+      <h1>Hello</h1>
     </div>
   );
 };
