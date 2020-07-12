262ed85e5bdb6924046bfee69a8ce1b741bbe75a
Mark Whang <hwhang0917@gmail.com>
2020-07-12 14:25:25 +0900

    [2.8] useAxios hook

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from "react";
+import useAxios from "./hooks/useAxios";
 
 const App = () => {
+  const { loading, data, error, refetch } = useAxios({
+    url: "https://yts.mx/api/v2/list_movies.json",
+  });
+  console.log(
+    `Loading: ${loading}\nError:${error}\nDate:${JSON.stringify(data)}`
+  );
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1>{data && data.status}</h1>
+      <h2>{loading && "Loading"}</h2>
+      <button onClick={refetch}>Refetch</button>
     </div>
   );
 };
